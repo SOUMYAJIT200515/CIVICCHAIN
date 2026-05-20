f52d936c01cf4a7b7e179d0f4014ce95c428b817
Soumyajit Saha <soumyajitsaha.miit@gmail.com>
2026-05-20 18:33:50 +0530

    Update API_BASE to use environment variable

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,4 +1,6 @@
-const API_BASE = "http://localhost:8080/api";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL 
+  ? `${process.env.NEXT_PUBLIC_API_URL}/api` 
+  : "http://localhost:8080/api";
 
 const CC_KEYS = { adminSession: 'cc_admin_session' };
 
